ddf9a65952a376e09d0f1be5e61b1fc40b7df4e5
fatimamammadova <fatimammmadova@gmail.com>
2024-01-16 01:47:25 +0400

    cashback

--- Yelo-Bank/src/js/cashback.js
+++ Yelo-Bank/src/js/cashback.js
@@ -12,43 +12,99 @@ async function getBrands() {
         let index=30
         let k=60
         moreBtn.addEventListener('click',() => {
-            getdata(index,k)
-            index=k
-            k+=30
+            showMoreBrands(data)
+           
         })
-
-        function getdata(first,two) {
-            for(let i=first;i<two;i++) {
-                if(data[i]) {
+        
+        function showMoreBrands(dataName) {
+            for(let i=index;i<k;i++) {
+                if(dataName[i]) {
                     if(i<=k) {
                         brandContainer.innerHTML += `<div class="brand-item col-3">
                         <a href="javascript:void(0)">
-                            <div class="brand-bg" style="background-image: url('${data[i].img}');"></div>
+                            <div class="brand-bg" style="background-image: url('${dataName[i].img}');"></div>
                             <div class="item-content">
                                 <div class="content">
-                                    <h3>${data[i].name.length > 20 ? data[i].name.slice(0,20) + `...` : data[i].name}</h3>
-                                    <p>${data[i].category.length > 25 ? data[i].category.slice(0,25) + `...` : data[i].category}</p>
+                                    <h3>${dataName[i].name.length > 20 ? dataName[i].name.slice(0,20) + `...` : dataName[i].name}</h3>
+                                    <p>${dataName[i].category.length > 25 ? dataName[i].category.slice(0,25) + `...` : dataName[i].category}</p>
                                 </div>
                                 <div class="discount">
-                                    ${data[i].discount}<span>%</span>
+                                    ${dataName[i].discount}<span>%</span>
                                 </div>
                             </div>
                         </a>
                         </div>`
                     } else {
-                        i=two
+                        i=k
                         brandContainer.innerHTML += ``
                     }
                 } else {
                     brandContainer.innerHTML += ``
                 }
             }
+            index=k
+            k+=30
         }
-        getdata(0,30)
+        
+        function shownBrands(dataName) {
+            for(let i=0;i<30;i++) {
+                brandContainer.innerHTML += `<div class="brand-item col-3">
+                <a href="javascript:void(0)">
+                    <div class="brand-bg" style="background-image: url('${dataName[i].img}');"></div>
+                    <div class="item-content">
+                        <div class="content">
+                            <h3>${dataName[i].name.length > 20 ? dataName[i].name.slice(0,20) + `...` : dataName[i].name}</h3>
+                            <p>${dataName[i].category.length > 25 ? dataName[i].category.slice(0,25) + `...` : dataName[i].category}</p>
+                        </div>
+                        <div class="discount">
+                            ${dataName[i].discount}<span>%</span>
+                        </div>
+                    </div>
+                </a>
+                </div>`  
+            }
+        }
 
-                
+        shownBrands(data)
+
         searchInputs.addEventListener("input", () => {
+            let inputValue = searchInputs.value
+            brandContainer.innerHTML=''
+            if(inputValue=="") {
+                shownBrands(data)
+                showMoreBrands(data)
+            }
+            const arr=[]
+            data.forEach(item => {
+                if(item.name.toLowerCase().includes(inputValue) && inputValue!=="") {
+                    if(brandContainer.children.length<=30) {
+                        brandContainer.innerHTML += `<div class="brand-item col-3">
+                            <a href="javascript:void(0)">
+                                <div class="brand-bg" style="background-image: url('${item.img}');"></div>
+                                <div class="item-content">
+                                    <div class="content">
+                                        <h3>${item.name.length > 20 ? item.name.slice(0,20) + `...` : item.name}</h3>
+                                        <p>${item.category.length > 25 ? item.category.slice(0,25) + `...` : item.category}</p>
+                                    </div>
+                                    <div class="discount">
+                                        ${item.discount}<span>%</span>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>`
+                    } else {
+                        arr.push(item)
+                    }
+                }
+            })
+
+            if(arr.length>0) {
+                console.log(arr.length)
+                shownBrands(arr)
+                showMoreBrands(arr)
+            }
            
+            
         })
 
     }
